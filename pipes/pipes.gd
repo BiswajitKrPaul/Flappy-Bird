830a24dce727cc98d00e14a142f4fa76b5a382a5
Biswajit Kumar Paul <biswajitkmrpl46@gmail.com>
2023-12-13 23:29:33 +0530

    icon changes done

--- pipes/pipes.gd
+++ pipes/pipes.gd
@@ -1,31 +1,34 @@
 extends Node2D
 
-const SCROLL_SPEED:float = 150
-var movePipes:bool = true
+const SCROLL_SPEED: float = 150
+var movePipes: bool = true
 @onready var point_collected_audio_player = $"Point Collected Audio Player"
 
+
 func _ready():
-	movePipes=true
+	movePipes = true
 	GameManager.on_game_end.connect(_on_game_over)
 
+
 func _process(delta):
-	if(movePipes)==true:
-		position.x-=delta*SCROLL_SPEED
+	if (movePipes) == true:
+		position.x -= delta * SCROLL_SPEED
+
 
 func _on_screen_exited():
 	queue_free()
 
+
 func _on_game_over():
-	movePipes=false
+	movePipes = false
 
 
 func _on_body_entered(body):
-	if(body.is_in_group(GameManager.group_plane))==true:
+	if (body.is_in_group(GameManager.group_plane)) == true:
 		body.die()
 
 
-
 func _on_body_exited(body):
-	if(body.is_in_group(GameManager.group_plane))==true:
+	if (body.is_in_group(GameManager.group_plane)) == true:
 		GameManager._increase_score()
 		point_collected_audio_player.play()
